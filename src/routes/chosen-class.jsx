0d986bf7fe0a6121005fd50fcce3da919d0a6315
Justin Castro <juscas13@gmail.com>
2023-02-07 11:53:37 -0600

    add starting equipment to chosen-class

--- src/routes/chosen-class.jsx
+++ src/routes/chosen-class.jsx
@@ -1,17 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 
-export const chosenClassLoader = async ({params}) => {
+export const chosenClassLoader = async ({ params }) => {
   const res = await fetch(
     `https://www.dnd5eapi.co/api/classes/${params.index}`
   );
   if (!res.ok) throw new Error("Something went Wrong");
   const data = await res.json();
   return { data };
-
 };
 export default function ChosenClass() {
-  const { data }  = useLoaderData();
+  const { data } = useLoaderData();
   // console.log(data);
   return (
     <>
@@ -44,7 +43,33 @@ export default function ChosenClass() {
           })}
         </div>
       </div>
+      <div className="starting-equipment-card">
+        <h2>Starting Equipment</h2>
+        <div className="starting-equipment">
+          {data.starting_equipment.map((equipment) => {
+            return <p>{equipment.equipment.name}</p>;
+          })}
+          <h3>Additional Starting Equipment Options</h3>
+          <div>
+            {data.starting_equipment_options.map((equipment) => {
+              return (
+                <div key={equipment.desc}>
+                  <p>{equipment.desc}</p>
+                  <div>
+                    {/* Map over equipment to get the names  */}
+                    {/* Below does not work */}
+                    {/* {equipment.from.options
+                      ? equipment.from.options.map((option) => {
+                          return <button>{option.count}</button>;
+                        })
+                      : <p/>} */}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
-
 }
