b53165b878dfd2749ed478125ff69ee43358845b
Justin Castro <juscas13@gmail.com>
2023-02-07 08:23:17 -0600

    add classNames, wrap in divs : chosen-class.jsx

--- src/routes/chosen-class.jsx
+++ src/routes/chosen-class.jsx
@@ -19,22 +19,31 @@ export default function ChosenClass() {
       <p>
         Starting Hit Die: <span className="hit-die">{data.hit_die}</span>
       </p>
-      <h2>Proficiency Choices</h2>
-      <p>{data.proficiency_choices[0].desc}</p>
-      <div className="proficiencyChoices">{data.proficiency_choices[0].from.options.map((option) => {
-        return <button key={option.item.index}>{ option.item.name }</button>
-      })}</div>
-      <h2>Proficiencies</h2>
-      <ul>
-        {data.proficiencies.map((proficiency) => {
-          return <li key={proficiency.index}>{ proficiency.name}</li>
-        })}
-      </ul>
-      <h2>Saving Throws</h2>
-      <div>{data.saving_throws.map((saving) => {
-        return <p key={saving.index}>{saving.name}</p>
-      }) }</div>
-
+      <div className="proficiency-choices-card">
+        <h2>Proficiency Choices</h2>
+        <p>{data.proficiency_choices[0].desc}</p>
+        <div className="proficiency-choices">
+          {data.proficiency_choices[0].from.options.map((option) => {
+            return <button key={option.item.index}>{option.item.name}</button>;
+          })}
+        </div>
+      </div>
+      <div className="proficiency-card">
+        <h2>Proficiencies</h2>
+        <ul className="proficiency-list">
+          {data.proficiencies.map((proficiency) => {
+            return <li key={proficiency.index}>{proficiency.name}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="saving-throws-card">
+        <h2>Saving Throws</h2>
+        <div className="saving-throws">
+          {data.saving_throws.map((saving) => {
+            return <p key={saving.index}>{saving.name}</p>;
+          })}
+        </div>
+      </div>
     </>
   );
 
